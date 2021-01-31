3dcb4342ba867cb133b1eb57321c8f918718adf4
Erico Takano <erico.takano@endurance.com>
2021-01-31 10:18:30 -0300

    update createaccount page

--- cypress/support/page-objects/my-account/createAccountPage.js
+++ cypress/support/page-objects/my-account/createAccountPage.js
@@ -6,7 +6,7 @@ const TITLE_CREATE_AN_ACCOUNT = 'h1'
 
 export const accessSite = () => {
   cy.visit(URL);
-  return cy.get(TITLE_CREATE_ACCOUNT).should("be.visible");
+  cy.get(TITLE_CREATE_ACCOUNT).should("be.visible");
 }
 
 export const fillLoginCredentials = (email) => {
@@ -20,4 +20,14 @@ export const submit = () => {
 export const getTitle = () => {
   return cy.get(TITLE_CREATE_AN_ACCOUNT);
 }
+
+export const accessCreateAccount = () => {
+  cy.visit(URL);
+  cy.get(TITLE_CREATE_ACCOUNT).should("be.visible");
+  cy.get(FLD_EMAIL).type(email);
+  cy.get(BTN_SUBMIT).should("be.visible").click();
+  cy.get(TITLE_CREATE_AN_ACCOUNT).should("be.visible");
+}
+
+
 
